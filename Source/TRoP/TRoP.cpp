cfb5f7b060228c71de413e1f903f80ff598fba27
mattmaniak <2378768-mattmaniak@users.noreply.gitlab.com>
2020-07-02 10:08:51 +0200

    Switch to C++

---
+++ Source/TRoP/TRoP.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TRoP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TRoP, "TRoP" );
---
+++ Source/TRoP/TRoPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TRoPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TROP_API ATRoPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
